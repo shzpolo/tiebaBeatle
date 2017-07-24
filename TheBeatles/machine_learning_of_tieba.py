6254a3054e315a272ab0b56cfb5b419c92643d72
Polo <shzpolo@gmail.com>
2017-07-24 11:52:16 +0800

    basicly is a complete version

--- TheBeatles/machine_learning_of_tieba.py
+++ TheBeatles/machine_learning_of_tieba.py
@@ -1,9 +1,15 @@
 from sklearn.tree import DecisionTreeClassifier
+import pymysql
 
 info_add = r'C:\Users\Polo\PycharmProjects\test\ml_res\info_list'
 sex_add = r'C:\Users\Polo\PycharmProjects\test\ml_res\sex_list'
+host = '172.17.60.108'
+user = 'hz'
+passwd = 'hz123456'
+db = 'test_hz'
 example_sample = [[0.1, 1300], [4.4, 1617], [2.3, 5932], [0.1, 88], [0.7, 10], [2.2, 47]]  # mmffmm
-
+info_list = []
+sex_list = []
 
 def fit(source, target):
     model = DecisionTreeClassifier()
@@ -15,20 +21,23 @@ def predict(model, sample):
     return model.predict(sample)
 
 
-def get_file_from_txt(file_add):
+def get_file_from_db():
     print('Reading file...')
-    f = open(file_add, 'r')
-    file = f.read()
-    print("File information is " + file)
-    return file
+    try:
+        cursor.execute("SELECT * FROM test_hz.tieba_db;")
+        result = cursor.fetchall()
+        for r in result:
+            info_set = []
+            info_set.append(r[2])
+            info_set.append(r[3])
+            info_list.append(info_set)
+            sex_list.append(r[4])
+    except Exception as e:
+        print(e)
 
-
-#def translate_info(file):
-
-
-
-info_list = get_file_from_txt(info_add)
-sex_list = get_file_from_txt(sex_add)
+conn = pymysql.connect(host='172.17.60.108', user=user, password=passwd, db=db, charset='UTF8')
+cursor = conn.cursor()
+get_file_from_db()
 
 tieba_sex_model = fit(info_list, sex_list)
 print(predict(tieba_sex_model, example_sample))
--- TheBeatles/tieba_name_find.py
+++ TheBeatles/tieba_name_find.py
@@ -1,12 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-url_pre = 'http://tieba.baidu.com/f/index/forumpark?pcn='
-url_bac = '&pci=429&ct=0&rn=20&pn='
-names = ['娱乐明星','爱综艺','追剧狂','看电影','体育小说','生活家','闲·趣','游戏','动漫宅','高校','地区','人文自然']
+url_pre = 'http://tieba.baidu.com/f/index/forumpark?cn=&ci=0&pcn='
+url_bac = '&pci=0&ct=1&st=new&pn='
+names = ['娱乐明星','电视节目','电视剧','电影','体育迷','小说','生活家','闲·趣','游戏','动漫宅','高等院校','地区','人文自然']
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0'}
+tieba_names = []
+
+print(str(12*30*19))
 
 for name in names:
-    res = requests(url_pre+name+url_bac, headers = header)
+    for i in range(30):
+        url = url_pre + name + url_bac + str(i)
+        res = requests.get(url, headers = header)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        all_name = soup.find_all('p', class_='ba_name')
+        for n in all_name:
+            tieba_names.append(n.text[:-1])
+
+print(tieba_names)
+print(len(tieba_names))
+
 
 
